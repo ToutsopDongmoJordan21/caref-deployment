832427e0a35d53d1393522f59366153a232f760d
ToutsopDongmoJordan <jordantoutsop472@yahoo.fr>
2022-04-07 15:49:50 +0100

    dernier ajustement

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -14,3 +14,13 @@ INSERT INTO accessoire(accessoire_name) VALUES('CD Player ');
 INSERT INTO accessoire(accessoire_name) VALUES('Central Locking');
 INSERT INTO accessoire(accessoire_name) VALUES('Crash Sensor');
 INSERT INTO accessoire(accessoire_name) VALUES('Leather Seats');
+
+INSERT INTO users VALUES (1,'toto@gmail.com','admin123',699028439,'admin');
+INSERT INTO USER_ROLES VALUES (1,3);
+
+
+INSERT INTO users VALUES (2,'toto2@gmail.com','mod123',6499028439,'mod1');
+INSERT INTO USER_ROLES VALUES (2,2);
+
+INSERT INTO users VALUES (3,'toto4@gmail.com','user123',679028439,'user1');
+INSERT INTO USER_ROLES VALUES (3,1);
